0df0ae0ea2ed9c410688955e7852674dbc261e3d
DAN (Duc Anh Nguyen) <geruk@geruk.me>
2014-02-08 06:12:29 -0500

    Added hover

--- static/js/main.js
+++ static/js/main.js
@@ -2,79 +2,14 @@
 var currentPosition = 0,
     score = 0;
 
-/**
-If image == null, then show text only
-*/
-function question(text, imagename) {
-	this.text = text;
-	this.imagename = imagename;
-}
-
-function makeQuestion(linkBank){
-	var questions = [];
-	for(var i = 0; i < linkBank.length; i ++){
-		questions.push(new question(linkBank[i], null));
-	}
-	return questions;
-}
-
-function makeRandomQuestions(linkBank){
-	var questions = [];
-	for (var i = 0 ; i < linkBank.length; i ++){
-		var newIn = Math.floor(Math.random()*(linkBank.length - i) + i);
-		var tmp = linkBank[i];
-		linkBank[i] = linkBank[newIn];
-		linkBank[newIn] = tmp;
-	}
-	var questions = [];
-	for (var i = 0; i < linkBank.length; i ++){
-		questions.push(new question(linkBank[i]));
-	}
-	return questions;
-}
-
-var linkBank  = ["Twitter.com", "Twitler.com", "facebook.com", "facelook.com", "google.com", "gocgler.com"];
-var questions = makeRandomQuestions(linkBank);
 
 var GRID_WIDTH = 4;
 var GRID_HEIGHT = 4;
-
-/*
-var CANVAS_WIDTH = 480;
-var CANVAS_HEIGHT = 320;
-var FPS = 30;
-
-var canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas>");
-var canvas = canvasElement.get(0).getContext("2d");
-canvasElement.appendTo('body');
-
-setInterval(function() {
-	update();
-	draw();
-}, 1000/FPS);
-
-var textX = 50;
-var textY = 50;
-
-function update() {
-	textX += 1;
-	textY += 1;
-}
-
-function draw() {
-	canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-	canvas.fillStyle = "#000";
-	canvas.fillText("Sup Bro!", textX, textY);
-}
-*/
 
 $(document).on( "ready", function() {
     var $body = $('body'),
         $gridBox = $('.grid-box'),
         $infoBox = $('.info-box');
-
-	var GRID_WIDTH = 4;
-	var GRID_HEIGHT = 4;
 
 	var matrix = $(".grid-box");
 	for (i = 0; i < GRID_WIDTH; i++) {
@@ -95,22 +30,78 @@ $(document).on( "ready", function() {
     var bodyHeight = $body.height();
     $gridBox.height(bodyHeight).width(bodyHeight);
     var $span3 = $('.span3');
-    console.log($span3.parent().parent().width());
     var spanWidth = $span3.parent().parent().width()*0.25;
     $span3.height(spanWidth).width(spanWidth);
-    x = $('.well').width();
-    console.log(x);
-    $('.well').height(x);
+    $('.well').height($('.well').width());
 
     $('.well').hover(function() {
     	var target = event.currentTarget;
         var targetId = parseInt($(target).attr('id').substring(8));
 
         if (currentPosition + 1 == targetId || currentPosition + 4 == targetId) {
+
+			var distance = 10;
+			var time = 250;
+
+			// tracker
+			var beingShown = false;
+			var shown = false;
+
+			var trigger = $('.trigger', this);
+			var popup = $('.popup', this).css('opacity', 0);
 			$(this).css('cursor','pointer');
+
+		      // don't trigger the animation again if we're being shown, or already visible
+		      if (beingShown || shown) {
+		        return;
+		      } else {
+		        beingShown = true;
+
+		        // reset position of popup box
+		        popup.css({
+		          top: -10,
+		          left: -33,
+		          width: 400,
+		          display: 'block' // brings the popup back in to view
+		        })
+
+		        // (we're using chaining on the popup) now animate it's opacity and position
+		        .animate({
+		          top: '-=' + distance + 'px',
+		          opacity: 1
+		        }, time, 'swing', function() {
+		          // once the animation is complete, set the tracker variables
+		          beingShown = false;
+		          shown = true;
+		        });
+		      }
 		}
+
 	}, function() {
-		$(this).css('cursor','auto');
+    	var target = event.currentTarget;
+        var targetId = parseInt($(target).attr('id').substring(8));
+        if (currentPosition + 1 == targetId || currentPosition + 4 == targetId) {
+			var popup = $('.popup', this).css('opacity', 0);
+			$(this).css('cursor','auto');
+
+
+
+			var distance = 10;
+			var time = 250;
+			var hideDelay = 500;
+		      
+		      // store the timer so that it can be cleared in the mouseover if required
+		      hideDelayTimer = setTimeout(function () {
+		        hideDelayTimer = null;
+		        popup.animate({
+		          top: '-=' + distance + 'px',
+		          opacity: 0
+		        }, time, 'swing', function () {
+		          // hide the popup entirely after the effect (opacity alone doesn't do the job)
+		          popup.css('display', 'none');
+		        });
+		      }, hideDelay);
+		  }
 	});
 
 
@@ -138,70 +129,6 @@ $(document).on( "ready", function() {
             });
         }
     });
-
-    $('.bubbleInfo').each(function () {
-	    // options
-	    var distance = 10;
-	    var time = 250;
-	    var hideDelay = 500;
-
-	    var hideDelayTimer = null;
-
-	    // tracker
-	    var beingShown = false;
-	    var shown = false;
-	    
-	    var trigger = $('.trigger', this);
-	    var popup = $('.popup', this).css('opacity', 0);
-
-	    // set the mouseover and mouseout on both element
-	    $([trigger.get(0), popup.get(0)]).mouseover(function () {
-	      // stops the hide event if we move from the trigger to the popup element
-	      if (hideDelayTimer) clearTimeout(hideDelayTimer);
-
-	      // don't trigger the animation again if we're being shown, or already visible
-	      if (beingShown || shown) {
-	        return;
-	      } else {
-	        beingShown = true;
-
-	        // reset position of popup box
-	        popup.css({
-	          top: -10,
-	          left: -33,
-	          width: 400,
-	          display: 'block' // brings the popup back in to view
-	        })
-
-	        // (we're using chaining on the popup) now animate it's opacity and position
-	        .animate({
-	          top: '-=' + distance + 'px',
-	          opacity: 1
-	        }, time, 'swing', function() {
-	          // once the animation is complete, set the tracker variables
-	          beingShown = false;
-	          shown = true;
-	        });
-	      }
-	    }).mouseout(function () {
-	      // reset the timer if we get fired again - avoids double animations
-	      if (hideDelayTimer) clearTimeout(hideDelayTimer);
-	      
-	      // store the timer so that it can be cleared in the mouseover if required
-	      hideDelayTimer = setTimeout(function () {
-	        hideDelayTimer = null;
-	        popup.animate({
-	          top: '-=' + distance + 'px',
-	          opacity: 0
-	        }, time, 'swing', function () {
-	          // once the animate is complete, set the tracker variables
-	          shown = false;
-	          // hide the popup entirely after the effect (opacity alone doesn't do the job)
-	          popup.css('display', 'none');
-	        });
-	      }, hideDelay);
-	    });
-	  });
 });
 
 /*
