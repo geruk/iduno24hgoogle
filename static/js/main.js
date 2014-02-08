6465257f56d80716d6c0e752e01493d8d2f7c7b4
Duc Nguyen <vietducnguyen.vn@gmail.com>
2014-02-07 22:17:26 -0500

    add canvas template

--- static/js/main.js
+++ static/js/main.js
@@ -9,3 +9,30 @@ function question(text, imagename) {
 var questions = [];
 questions.push(new question("Twitter.com", null));
 questions.push(new question("Twitler.com", null));
+
+var CANVAS_WIDTH = 480;
+var CANVAS_HEIGHT = 320;
+var FPS = 30;
+
+var canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas>");
+var canvas = canvasElement.get(0).getContext("2d");
+canvasElement.appendTo('body');
+
+setInterval(function() {
+	update();
+	draw();
+}, 1000/FPS);
+
+var textX = 50;
+var textY = 50;
+
+function update() {
+	textX += 1;
+	textY += 1;
+}
+
+function draw() {
+	canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+	canvas.fillStyle = "#000";
+	canvas.fillText("Sup Bro!", textX, textY);
+}
