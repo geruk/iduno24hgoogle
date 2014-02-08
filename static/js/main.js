dbd28d0d6c226c786721a52e7f5c4d88d89dc014
DAN (Duc Anh Nguyen) <geruk@geruk.me>
2014-02-07 22:05:00 -0500

    Added first question stuff for JS

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,11 @@
+/**
+If image == null, then show text only
+*/
+function question(text, imagename) {
+	this.text = text;
+	this.imagename = imagename;
+}
 
+var questions = [];
+questions.push(new question("Twitter.com", null));
+questions.push(new question("Twitler.com", null));
