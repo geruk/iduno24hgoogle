abd4e4263ddcdf80484002636810b4a584248433
Vu Bui <buidinhvu@gmail.com>
2014-02-08 01:49:46 -0500

    Add move methods

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,40 @@
+// hold current player position
+var currentPosition = 0;
+
+/**
+If image == null, then show text only
+*/
+function question(text, imagename) {
+	this.text = text;
+	this.imagename = imagename;
+}
+
+function makeQuestion(linkBank){
+	var questions = [];
+	for(var i = 0; i < linkBank.length; i ++){
+		questions.push(new question(linkBank[i], null));
+	}
+	return questions;
+}
+
+function makeRandomQuestions(linkBank){
+	var questions = [];
+	for (var i = 0 ; i < linkBank.length; i ++){
+		var newIn = Math.floor(Math.random()*(linkBank.length - i) + i);
+		var tmp = linkBank[i];
+		linkBank[i] = linkBank[newIn];
+		linkBank[newIn] = tmp;
+	}
+	var questions = [];
+	for (var i = 0; i < linkBank.length; i ++){
+		questions.push(new question(linkBank[i]));
+	}
+	return questions;
+}
+
+var linkBank  = ["Twitter.com", "Twitler.com", "facebook.com", "facelook.com", "google.com", "gocgler.com"];
+var questions = makeRandomQuestions(linkBank);
+
 var GRID_WIDTH = 4;
 var GRID_HEIGHT = 4;
 
@@ -60,3 +97,15 @@ $(document).on( "ready", function() {
     console.log(x);
     $('.well').height(x);
 });
+
+function moveRight(currentPosition) {
+    $('#element-' + currentPosition + ' img').fadeIn();
+    currentPosition += 1;
+    $('#element-' + currentPosition + ' img').fadeOut();
+}
+
+function moveBottom(currentPosition) {
+    $('#element-' + currentPosition + ' img').fadeIn();
+    currentPosition += 4;
+    $('#element-' + currentPosition + ' img').fadeOut();
+}
